4ff36aeca48a1eaf99c04741a5d364bb4fd42f3d
Janos Zab <janos.zab@swstudio.hu>
2015-03-14 16:54:55 +0100

    typo fix

--- messages/hu/maintenance.php
+++ messages/hu/maintenance.php
@@ -3,5 +3,5 @@ return [
     'Maintenance mode' => 'Karabantartás',
     'Sorry, this page is currently not available.' => 'Sajnáljuk, de az oldal jelenleg nem elérhető ',
     'This page is currently in a maintenance mode. Usually this takes 5-15 minutes.' => 'Az oldalon karbantartást végzünk',
-    'Visit us again soon.' => 'Látogasson vissza, hamarosan újra elérhtőel leszünk.',
+    'Visit us again soon.' => 'Látogasson vissza, hamarosan újra elérhtőek leszünk.',
 ];
